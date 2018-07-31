2f22043b9720c4b5161632976e12b9adb4a18f84
asifsaj <35814758+asifsaj@users.noreply.github.com>
2018-07-31 10:40:18 -0500

    Add files via upload

---
+++ Assets/C#Scripts/ColliderPushBall.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderPushBall : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private Vector2 vel;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    //Rotation of the Ball
+    void FixedUpdate()
+    {
+        transform.Rotate(0, 0, 5);
+    }
+
+    //Collisions
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        if (coll.gameObject.tag == "Player")
+        {
+            if(Random.Range(0, 2) == 1)
+            {
+                vel.x = rb2d.velocity.x - 4;
+                vel.y = (rb2d.velocity.y + 8);
+            }
+            else
+            {
+                vel.x = rb2d.velocity.x + 4;
+                vel.y = (rb2d.velocity.y + 8);
+            }
+            rb2d.velocity = vel;
+        }
+
+        if (coll.gameObject.tag == "Wall")
+        {
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y);
+            rb2d.velocity = vel;
+        }
+    }
+
+}
---
+++ Assets/C#Scripts/Player_Controller.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    public float playerSpeed = 10;  //allows us to be able to change speed in Unity
+    public Vector2 jumpHeight;
+
+    //Ground Check Variables for Double Jumping
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded = false;
+    private bool doubleJumped;
+
+    // Update is called once per frame
+    public float panSpeed = 20f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //Checked every second or so
+    void FixedUpdate()
+    {
+       grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround); // Grounded check
+    }
+
+
+    void Update()
+    {
+        //------------------------------------------------
+        //Movement
+        Vector3 pos = transform.position;
+
+        //Moving Left and Right and Down
+        if (Input.GetKey("d"))
+        {
+            pos.x += 10 * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= 10 * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            if(grounded == false)
+                pos.y -= 10 * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+        transform.Translate(playerSpeed * Time.deltaTime, 0f, 0f);  //makes player run
+        //-------------------------------------------------------------
+        //Jumping and Double Jumping and Grounded Check
+        if (grounded)
+            doubleJumped = false;
+
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && !doubleJumped && !grounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+            doubleJumped = true;
+        }
+        
+    }
+}
+
+
