1897bcfd2e0594a17bdf1559fa242c8d0f1be7a2
asifsaj <35814758+asifsaj@users.noreply.github.com>
2018-08-02 07:51:59 -0500

    Main Game

--- Assets/C#Scripts/ColliderPushBall.cs
+++ Assets/C#Scripts/ColliderPushBall.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class ColliderPushBall : MonoBehaviour
 {
+    
     private Rigidbody2D rb2d;
     private Vector2 vel;
+
+    public GameObject explosion;
+
 
     // Use this for initialization
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-
     }
 
     //Rotation of the Ball
     void FixedUpdate()
     {
         transform.Rotate(0, 0, 5);
+
     }
 
     //Collisions
@@ -37,6 +41,8 @@ public class ColliderPushBall : MonoBehaviour
                 vel.y = (rb2d.velocity.y + 8);
             }
             rb2d.velocity = vel;
+            //Add to the score
+            ScoreManager.score += 1;
         }
 
         if (coll.gameObject.tag == "Wall")
@@ -44,6 +50,11 @@ public class ColliderPushBall : MonoBehaviour
             vel.x = rb2d.velocity.x;
             vel.y = (rb2d.velocity.y);
             rb2d.velocity = vel;
+        }
+        //Destroy the ball and end the game if ball collides with the Ground
+        if (coll.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
         }
     }
 
---
+++ Assets/C#Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;        // The player's score.
+
+
+    public Text scoreText;                      // Reference to the Text component.
+
+
+    void Start()
+    {
+        // Set up the reference.
+        scoreText = GetComponent<Text>();
+
+        // Reset the score.
+        score = 0;
+    }
+
+
+    void Update()
+    {
+        // Set the displayed text to be the word "Score" followed by the score value.
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
